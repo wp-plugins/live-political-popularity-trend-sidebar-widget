0a17065c0b54fd25d3ba7c1651cd9a74c1a4ea9c
magadi <magadi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-22 23:51:15 +0000

    git-svn-id: https://plugins.svn.wordpress.org/live-political-popularity-trend-sidebar-widget/tags/0.1@314742 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,44 @@
+=== Live Political Popularity Comparison Chart Generator Widget ===
+
+Contributors: personalmoneystore, Director of Personal Money Store: David Johnston
+Donate link:  If you like this plugin and would like to donate, the best donation we could ask for at this time is your positive comments! 
+Tags: compare politicians, political popularity trends, political popularity graph, political popularity chart, political popularity checker, live politics, politics, political widget, politics widget, political plugin, political wordpress widget, politics wordpress widget, political wordpress plugin, politics wordpress plugin, widget, plugin, wordpress widget, wordpress sidebar widget, wordpress plugin, 
+Requires at least: 2.9.2
+Tested up to: 3.0.1
+Stable tag:  trunk
+
+A sidebar widget graph that dynamically updates providing the latest info on 1 or 2 politicians that admin inserted as default or that user selects
+
+== Description ==
+Compare politicians in a side by side contest where Google's search statistics show who is more popular. Not many people know that Google tracks what people are searching for and the volume of those searches. 
+It is easy to compare politicians in a visual way with this graph generator and see who is more popular without resorting to votes. 
+You will be pretty shocked at who is more popular then who, besides being thoroughly entertained. 
+This tool will be something your political audience will appreciate using and playing around with. 
+Install it and see your web site's interaction sky rocket!
+
+== Installation ==
+1. Upload `wp-ppw.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to Widgets (under Appearance) and drag "WP Political Popularity Widget" to the sidebar.
+4. Open "WP Political Popularity Widget" in sidebar and fill in the title, the first politician's name and the second politician's name.
+5. Save and close sidebar widget.
+== Changelog ==
+For this version 1.0.1, we have changed location where the plugin zipfile is being hosted.
+Also added the screenshot image to the wordpress plugin directory.
+== Frequently Asked Questions ==
+Can I make a suggestion for this plugin?
+Of course, please visit our Politician Popularity Widget page http://personalmoneystore.com/moneyblog/live-political-popularity-comparison-chart-generator-widget/ and leave a suggestion under, "Post a new comment."
+== Upgrade Notice ==
+ 11/20/2010 - released Live Political Popularity Comparison Chart Generator Widget Verison 1.0.1.  We have changed 
+the screenshoot.
+== Screenshots ==
+1. Screenshot of widget.
+http://personalmoneystore.com/wp-content/uploads/2010/11/screenshoot-2.jpg
+== Credits ==
+The image is compiled by Google and this gadget author has no control over what is displayed nor the accuracy of it.
+== License ==
+This Live Political Popularity Trend Widget is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. 
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the   GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+The image is compiled by Google and this gadget author has no control over what is displayed or the accuracy of it.
---
+++ wp-ppw.php
@@ -0,0 +1,163 @@
+<?php
+
+
+
+/*
+
+
+Plugin Name: Live Political Popularity Comparison Chart Generator Widget
+
+Version: 1.0.1
+
+
+Description:Compare politicians in a side by side contest where Google's search statistics show who is more popular. 
+Not many people know that Google tracks what people are searching for and the volume of those searches. 
+It is easy to compare politicians in a visual way with this graph generator and see who is more popular without resorting to votes. 
+You will be pretty shocked at who is more popular then who, besides being thoroughly entertained. 
+This tool will be something your political audience will appreciate using and playing around with. 
+Install it and see your web site's interaction sky rocket!
+
+Author: Director of Personal Money Store: David Johnston
+Author URI: http://wordpress.org/support/profile/personalmoneystore/
+Plugin URI: http://personalmoneystore.com/moneyblog/live-political-popularity-comparison-chart-generator-widget
+
+
+*/
+
+
+  /*
+   Copyright 2010  Director of Personal Money Store: David Johnston  (email : webmaster@personalmoneystore.com)
+   The image is compiled by Google and this gadget author has no control over what is displayed or the accuracy of it.
+
+   This program is free software; you can redistribute it and/or modify
+
+   it under the terms of the GNU General Public License as published by
+
+   the Free Software Foundation; either version 2 of the License, or
+
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+   */
+
+
+global $wp_version;
+
+
+	$exit_msg = 'WP Political Popularity Widget requires WordPress 2.6 or newer. <a href="http://codex.wordpress.org/Upgrading_WordPress">Please update!</a>';
+
+	if (version_compare($wp_version, "2.6", "<")) {
+
+      exit($exit_msg);
+
+	}
+
+$wp_ppw_plugin_url =  trailingslashit( WP_PLUGIN_URL.'/'. dirname( plugin_basename(__FILE__) ));
+function WPPpw_WidgetControl()
+{
+
+	// get saved options
+	$options = get_option('wp_ppw');
+	// handle user input
+	if ($_POST["ppw_submit"]) {
+
+		// retireve ppw title from the request
+
+		$options['ppw_title'] = strip_tags(stripslashes($_POST["ppw_title"]));
+
+		$options['first_name'] = strip_tags(stripslashes($_POST["first_name"]));
+
+		$options['second_name'] = strip_tags(stripslashes($_POST["second_name"]));
+
+		$options['enable_checkbox'] = strip_tags(stripslashes($_POST["enable_checkbox"]));
+
+ 		// update the options to database
+
+        update_option('wp_ppw', $options);
+
+	}
+
+	$title = $options['ppw_title'];
+
+	$first_name = $options['first_name'];
+
+	$second_name = $options['second_name'];
+
+	$enable_checkbox = $options['enable_checkbox'];
+
+	// print out the widget control, links to widget control    
+
+	include('wp-ppw-widget-control.php');
+
+}  
+
+function WPPpw_Widget($args = array())
+
+{ 
+	// extract the parameters
+      extract($args);
+
+	// get our options
+	$options = get_option('wp_ppw');
+	$title = $options['ppw_title'];
+	$first_name = $options['first_name'];
+	$second_name = $options['second_name'];
+	$enable_checkbox = $options['enable_checkbox'];
+	// print the theme compatibility code
+	echo $before_widget;
+
+	echo $before_title . $title . $after_title;
+
+	// include our widget
+	include('wp-ppw-widget.php');
+	echo $after_widget;
+
+}
+
+//loads from the start
+function WPPpw_Init()
+{
+
+	// register widget
+	register_sidebar_widget('WP Political Popularity Widget', 'WPPpw_Widget');
+	// register widget control
+	register_widget_control('WP Political Popularity Widget', 'WPPpw_WidgetControl');    
+}
+add_action('init', 'WPPpw_Init');
+//links to css
+add_action('wp_head', 'WPPpw_HeadAction');
+function WPPpw_HeadAction()
+{
+	global $wp_ppw_plugin_url;     
+    echo '<link rel="stylesheet" href="' . $wp_ppw_plugin_url . '/wp-ppw.css" type="text/css" />';
+
+}
+
+//links to javascript
+
+add_action('wp_print_scripts', 'WPPpw_ScriptsAction');
+
+function WPPpw_ScriptsAction()
+
+{
+
+	if (!is_admin()) {
+	global $wp_ppw_plugin_url;
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('jquery-form');
+	wp_enqueue_script('wp_ppw_script', $wp_ppw_plugin_url . '/wp-ppw.js', array('jquery', 'jquery-form'));
+	}
+
+}
+
+?>
